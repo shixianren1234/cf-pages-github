eebdb59de05fe654ed774688dbfff44602c06ee8
shixianren1234 <shixianren1234@163.com>
2025-08-04 14:56:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const _url = new URL(request.url);
     _url.hostname = _url.pathname.startsWith("/gh/")
-      ? "cdn.jsdelivr.net"
+      ? "github.com"
       : "www.baidu.com";
     const req = new Request(_url, request);
     return fetch(req);
